5f7384f16f9431022cd90bb71b84897998c5445b
Jeremy Kahn <jeremyckahn@gmail.com>
2012-03-24 16:28:28 -0700

    Adds some event hooks.

--- src/jquery.dragon.js
+++ src/jquery.dragon.js
@@ -1,7 +1,35 @@
 ;(function ($) {
 
+  /**
+   * Options:
+   *
+   *   - noCursor
+   */
   $.fn.dragon = function (opts) {
-    console.log(this);
+    opts = opts || {};
+    this.css('position', 'absolute');
+
+    if (!opts.noCursor) {
+      this.css('cursor', 'pointer');
+    }
+
+    initDragonEls(this, opts);
   };
+
+  function initDragonEls ($els, opts) {
+    $els.each(function (i, el) {
+      var $el = $(el);
+
+      $el.on('mousedown', onMouseDown);
+      $el.on('mouseup', onMouseUp);
+
+    });
+  }
+
+  function onMouseDown (evt) {
+  }
+
+  function onMouseUp (evt) {
+  }
 
 } (jQuery));
