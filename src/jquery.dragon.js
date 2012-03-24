d90f2feaba7f1428565e89f634c4b0a611a53d6a
Jeremy Kahn <jeremyckahn@gmail.com>
2012-03-24 16:51:45 -0700

    Even more hooks.

--- src/jquery.dragon.js
+++ src/jquery.dragon.js
@@ -1,5 +1,7 @@
 ;(function ($) {
 
+  var $win = $(window);
+
   /**
    * Options:
    *
@@ -19,17 +21,36 @@
   function initDragonEls ($els, opts) {
     $els.each(function (i, el) {
       var $el = $(el);
-
-      $el.on('mousedown', onMouseDown);
-      $el.on('mouseup', onMouseUp);
-
+      $el.on('mousedown', $.proxy(onMouseDown, $el));
     });
   }
 
   function onMouseDown (evt) {
+    var onMouseUpInstance = $.proxy(onMouseUp, this);
+    var onMouseMoveInstance = $.proxy(onMouseMove, this);
+    var initialPosition = this.position();
+    this.data('dragon', {
+      'onMouseUp': onMouseUpInstance
+      ,'onMouseMove': onMouseMoveInstance
+      ,'left': initialPosition.left
+      ,'top': initialPosition.top
+    });
+
+    $win.on('mouseup', onMouseUpInstance);
+    $win.on('blur', onMouseUpInstance);
+    $win.on('mousemove', onMouseMoveInstance);
   }
 
   function onMouseUp (evt) {
+    var data = this.data('dragon');
+    $win.off('mouseup', data.onMouseUp);
+    $win.off('blur', data.onMouseUp);
+    $win.off('mousemove', data.onMouseMove);
+    delete data.onMouseUp;
+    delete data.onMouseMove;
+  }
+
+  function onMouseMove (evt) {
   }
 
 } (jQuery));
