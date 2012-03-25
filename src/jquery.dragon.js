f0d194b08d0b74378e7fa801d10500e8f6e89e84
Jeremy Kahn <jeremyckahn@gmail.com>
2012-03-25 16:02:30 -0700

    Added some events.

--- src/jquery.dragon.js
+++ src/jquery.dragon.js
@@ -6,7 +6,7 @@
   /**
    * Options:
    *
-   *   - noCursor
+   *   @param {boolean} noCursor Prevents the drag cursor from being "move"
    */
   $.fn.dragon = function (opts) {
     initDragonEls(this, opts || {});
@@ -15,7 +15,6 @@
   function initDragonEls ($els, opts) {
     $els.css('position', 'absolute');
     $els.attr('draggable', 'true');
-    //$els.attr('ondragstart', 'return false;');
     $els.on('dragstart', preventDefault);
 
     if (!opts.noCursor) {
@@ -33,17 +32,24 @@
           'top': top
           ,'left': left
         })
-        .on('mousedown', $.proxy(onMouseDown, $el));
+        .data('dragon', {})
+        .on('mousedown', $.proxy(onMouseDown, $el))
+        .on('dragon-dragstart', $.proxy(opts.onDragStart || $.noop, $el))
+        .on('dragon-drag', $.proxy(opts.onDrag || $.noop, $el))
+        .on('dragon-dragend', $.proxy(opts.onDragEnd || $.noop, $el));
     });
   }
 
   function onMouseDown (evt) {
+    var data = this.data('dragon');
+    var wasAlreadyDragging = data.isDragging;
     var onMouseUpInstance = $.proxy(onMouseUp, this);
     var onMouseMoveInstance = $.proxy(onMouseMove, this);
     var initialPosition = this.position();
     this.data('dragon', {
       'onMouseUp': onMouseUpInstance
       ,'onMouseMove': onMouseMoveInstance
+      ,'isDragging': true
       ,'left': initialPosition.left
       ,'top': initialPosition.top
       ,'grabPointX': initialPosition.left - evt.pageX
@@ -56,16 +62,19 @@
       .on('mousemove', onMouseMoveInstance);
 
     $doc.on('selectstart', preventSelect);
+    this.trigger('dragon-dragstart');
   }
 
   function onMouseUp (evt) {
     var data = this.data('dragon');
+    data.isDragging = false;
     $win.off('mouseup', data.onMouseUp);
     $win.off('blur', data.onMouseUp);
     $win.off('mousemove', data.onMouseMove);
     $doc.off('selectstart', preventSelect);
     delete data.onMouseUp;
     delete data.onMouseMove;
+    this.trigger('dragon-dragend');
   }
 
   function onMouseMove (evt) {
@@ -74,6 +83,8 @@
       'left': evt.pageX + data.grabPointX
       ,'top': evt.pageY + data.grabPointY
     });
+
+    this.trigger('dragon-drag');
   }
 
   // This event handler fixes some craziness with the startselect event breaking
