739d6d79179118a8b75d742502fde1eb0cf5ec7c
Jeremy Kahn <jeremyckahn@gmail.com>
2012-06-02 14:59:29 -0700

    Fixes version number.

--- src/jquery.dragon.js
+++ src/jquery.dragon.js
@@ -1,6 +1,6 @@
 /**
  * jQuery Dragon.  It's a dragging plugin!
- *   v0.1.1
+ *   v0.1.2
  *   By Jeremy Kahn (jeremyckahn@gmail.com)
  *   MIT License.
  *   For more info: https://github.com/jeremyckahn/dragon
