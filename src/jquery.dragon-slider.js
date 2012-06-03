dd03d332079b47a8a9d00bcd9a66ddae77a64646
Jeremy Kahn <jeremyckahn@gmail.com>
2012-06-03 00:36:24 -0700

    Adds slider value get/set, keyboard controls.

--- src/jquery.dragon-slider.js
+++ src/jquery.dragon-slider.js
@@ -17,9 +17,16 @@
   // CONSTANTS
   var DEFAULTS = {
     'width': 250
+    ,'increment': .02
   };
+  var KEY_RIGHT = 39;
+  var KEY_LEFT = 37;
 
 
+  /**
+   * @param {Object=} opts
+   *   @param {number} width
+   */
   $.fn.dragonSlider = function (opts) {
     opts = opts || {};
     var defaultsCopy = $.extend({}, DEFAULTS);
@@ -27,27 +34,72 @@
   };
 
 
-  function initDragonSliderEls ($els, opts) {
+  /**
+   * @param {number} val Between 0 and 1.
+   */
+  $.fn.dragonSliderSet = function (val) {
+    val = Math.min(1, val);
+    val = Math.max(0, val);
+    var $handle = this.find('.dragon-slider-handle');
+    var scaledVal = val * (this.width() - $handle.outerWidth());
+    $handle.css('left', scaledVal);
+  };
 
+
+  /**
+   * @return {number} Between 0 and 1.
+   */
+  $.fn.dragonSliderGet = function () {
+    var $handle = this.find('.dragon-slider-handle');
+    var left = $handle.position().left;
+    return left / (this.width() - $handle.outerWidth());
+  };
+
+
+  /**
+   * @param {jQuery} $els
+   * @param {Object} opts
+   */
+  function initDragonSliderEls ($els, opts) {
     $els.each(function (i, el) {
       var $el = $(el);
       var $handle = createDragHandle($el);
       $el
         .addClass('dragon-slider')
         .width(opts.width)
-        .append($handle);
+        .append($handle)
+        .data('dragon-slider', $.extend({}, opts));
     });
   }
 
 
-  function createDragHandle (container) {
+  /**
+   * @param {jQuery} $container
+   */
+  function createDragHandle ($container) {
     var $handle = $(document.createElement('BUTTON'));
     $handle.addClass('dragon-slider-handle');
     $handle.dragon({
-      'within': container
+      'within': $container
     });
+    $handle.on('keydown', onHandleKeydown);
 
     return $handle;
+  }
+
+
+  function onHandleKeydown (ev) {
+    var $el = $(this);
+    var $parent = $el.parent();
+    var current = $parent.dragonSliderGet();
+    var increment = $parent.data('dragon-slider').increment;
+    var key = ev.which;
+
+    if (key === KEY_LEFT) {
+      $parent.dragonSliderSet(current - increment);
+    } else if (key === KEY_RIGHT) {
+      $parent.dragonSliderSet(current + increment);
+    }
   }
 
 } (this.jQuery));
