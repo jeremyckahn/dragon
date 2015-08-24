5c17b33d4bee2c560a1b2f523f2caa3f04bc7b61
Tair Assimov <tair@assimov.net>
2015-08-24 10:47:15 +0200

    Add isDragging css class

--- src/jquery.dragon.js
+++ src/jquery.dragon.js
@@ -226,6 +226,7 @@
     // Remove the "draggable" attribute so that text within the element can be
     // selected when the element is not being dragged.
     $el.attr('draggable', 'false');
+    $el.removeClass('isDragging');
 
     if (isTouch) {
       $doc.off('touchend', data.onTouchEnd)
@@ -325,6 +326,7 @@
     }
 
     $el
+      .addClass('isDragging')
       .css(ZERO_OUT_RIGHT_AND_BOTTOM)
       .offset(newCoords);
     fire('drag', $el, evt);
